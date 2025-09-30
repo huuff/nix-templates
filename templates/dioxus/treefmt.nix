7c8478cad32fc03b8cb5f0974f3f3c50dd2fb05f
Haf <haf@protonmail.ch>
2025-09-30 20:27:58 +0200

    feat: dx fmt to dioxus projects

--- templates/dioxus/treefmt.nix
+++ templates/dioxus/treefmt.nix
@@ -1,4 +1,5 @@
-_: {
+{ pkgs, rustPkgs, ... }:
+{
   projectRootFile = "flake.nix";
 
   programs = {
@@ -7,4 +8,44 @@ _: {
     taplo.enable = true;
     yamlfmt.enable = true;
   };
+
+  settings = {
+    formatter = {
+      dxfmt = {
+        # XXX: copy-pasted this approach from the treefmt impl for packer
+        # CONTRIB: add this to treefmt-nix, maybe make it better
+        command = pkgs.writeShellApplication {
+          name = "treefmt-nix-dx-fmt-wrapper";
+          runtimeInputs = [ rustPkgs ];
+          text =
+            let
+              dx = "${pkgs.dioxus-cli}/bin/dx";
+            in
+            ''
+              set -eu
+
+              for file in "$@"; do
+                # we have to do this incredibly weird shit because apparently all dx fmt commands
+                # modify the file (update mtime), and that will make the treefmt check mode fail
+                # (in pre-commit for example).
+                # so we just put the formatted output in some other file, and put it back only if we're
+                # sure it's changed
+                tmp="$file.FORMATTED"
+                ${dx} fmt --file - <"$file" > "$tmp"
+                if ! cmp -s "$file" "$tmp"; then
+                  mv -f "$tmp" "$file"
+                else
+                  rm "$tmp"
+                fi
+              done
+
+              exit "''${rc:-0}"
+            '';
+        };
+
+        includes = [ "*.rs" ];
+      };
+    };
+  };
+
 }
